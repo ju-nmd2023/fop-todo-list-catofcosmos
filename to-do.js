9b44b5fdb66c2159a211090f5e13e07a8b58fcfe
Caroline K <klca23yg@student.ju.se>
2024-04-18 18:58:26 +0200

    adding task works

--- to-do.js
+++ to-do.js
@@ -1,23 +1,49 @@
-// retrieve task from local storage or inicialize empty array 
+// retrieve task from local storage or inicialize empty array
 
-let todo = JSON.parse(localStorage.getItem
-    ("todo")) || [];
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
+const textInputElement = document.getElementById("textInput");
+const taskListElement = document.getElementById("taskList");
+const addBtn = document.querySelector("addBtn");
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   addBtn.addEventListener("click", addTaskToNoteContainer);
+// });
 
-const textInput = document.getElementById("textInput");
-const taskList = document.getElementById("taskList");
-const addBtn = document.querySelector(".btn");
-const deleteBtn = document.getElementById("deleteBtn");
+function addTaskToNoteContainer() {
+  let inputText = textInputElement.value.trim();
+  console.log("Hello");
+  if (inputText !== "") {
+    let task = document.createElement("li");
+    task.classList.add("task");
 
-document.addEventListener ("DOMContentLoaded",
-function (){
-    addBtn.addEventListener("click", addTaskToNoteContainer);
+    let taskNode = document.createTextNode(inputText);
 
-});
-function addTaskToNoteContainer (){
-    blah
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onclick = function () {
+      if (checkbox.checked) {
+        task.classList.add("task-done");
+      } else {
+        task.classList.remove("task-done");
+      }
+    };
+
+    task.appendChild(checkbox);
+    task.appendChild(taskNode);
+
+    taskListElement.appendChild(task);
+
+    inputElement.value = "";
+  }
 }
+
+  const deleteBtn = document.getElementById("deleteBtn");
+  deleteBtn.addEventListener();
+  function deleteTasks() {
+    while (taskListElement.firstChild)
+      taskListElement.removeChild(taskListElement.firstChild);
+  }
 
 // function addTaskToNoteContainer() {
 //     console.log("Button clicked");
